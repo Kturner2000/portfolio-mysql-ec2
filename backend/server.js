02afbbd7ce5f7869a45a06abf09bd7145bbe84cf
kylie <ksjturner00@gmail.com>
2025-01-11 20:25:06 +0000

    create user get and post routes

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-// const bodyParser = require('body-parser');
 const userRoutes = require('./routes/User.routes');
 require('dotenv').config();
-const db = require('./lib/db');  // Import the database connection pool
+var bodyParser = require('body-parser')
 
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 
 // Routes
