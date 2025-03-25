9a0f2266e46d8190bba4c21ee9fc59bb13ec9dc6
Kturner2000 <100411034+Kturner2000@users.noreply.github.com>
2025-03-25 12:54:39 +0100

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,9 @@ app.use('/api', userRoutes);
 app.use('/api', photoRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
   app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../../../frontend/dist", "index.html"));
+      res.sendFile(path.join(__dirname, "../../frontend/dist", "index.html"));
   });
 }
 
